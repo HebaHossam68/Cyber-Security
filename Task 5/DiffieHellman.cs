5b598b3b5828e3c34aabbfae447eff778d6d45a0
Heba Hossam <95322482+HebaHossam68@users.noreply.github.com>
2024-05-03 20:34:53 +0300

    Add files via upload

---
+++ Task 5/DiffieHellman.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SecurityLibrary.DiffieHellman
+{
+
+
+    public class DiffieHellman
+    {
+
+      public  int power(int f, int s, int sf)
+        {
+            //throw new NotImplementedException();
+            int res = 1;
+            for(int i=0;i<s;i++)
+            {
+                res = (res * f) % sf;
+            }
+            return res;
+
+            
+        }
+
+       public List<int> GetKeys(int q, int alpha, int xa, int xb)
+        {
+
+            //throw new NotImplementedException();
+           int ya = power(alpha, xa, q);
+           int yb = power(alpha, xb, q);
+           int k1 = power(yb, xa, q);
+           int k2 = power(ya, xb, q);
+
+            List<int> result = new List<int>();
+            result.Add(k1);
+            result.Add(k2);
+
+            return result;
+
+            
+        }
+    }
+}
